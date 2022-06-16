6a784789111ac604f1f6bd39c5b0ba4f81b1ef2d
luckylaurens15 <lucky.laurens@binus.ac.id>
2022-06-16 16:00:29 +0700

    JS jadi

--- buildPC.js
+++ buildPC.js
@@ -3,7 +3,7 @@ var index = 2;
 
 
 function next(){
-    console.log("test");
+    console.log(index);
     if (index == 2){
         document.getElementById("stages").innerHTML = 
         `<div class="display" style="height: 700px;" id="stages">
@@ -102,5 +102,348 @@ function next(){
     </div>
         `;
     }
-    next+=1;
+    else if (index == 4){
+        document.getElementById("stages").innerHTML = 
+        `<div class="display" style="height: 700px;" id="stages">
+        <p class="title"> Choose Your Hard Drive</p>
+        <ul>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Hard Drive 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Hard Drive 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Hard Drive 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Hard Drive 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+        </ul>
+    </div>
+        `;
+    }
+    else if (index == 5){
+        document.getElementById("stages").innerHTML = 
+        `<div class="display" style="height: 700px;" id="stages">
+        <p class="title"> Choose Your SSD</p>
+        <ul>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>SSD 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>SSD 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>SSD 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>SSD 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+        </ul>
+    </div>
+        `;
+    }
+    else if (index == 6){
+        document.getElementById("stages").innerHTML = 
+        `<div class="display" style="height: 700px;" id="stages">
+        <p class="title"> Choose Your RAM</p>
+        <ul>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>RAM 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>RAM 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>RAM 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>RAM 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+        </ul>
+    </div>
+        `;
+    }
+    else if (index == 7){
+        document.getElementById("stages").innerHTML = 
+        `<div class="display" style="height: 700px;" id="stages">
+        <p class="title"> Choose Your Monitor</p>
+        <ul>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Monitor 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Monitor 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Monitor 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Monitor 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+        </ul>
+    </div>
+        `;
+    }
+    else if (index == 8){
+        document.getElementById("stages").innerHTML = 
+        `<div class="display" style="height: 700px;" id="stages">
+        <p class="title"> Choose Your Casing</p>
+        <ul>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Casing 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Casing 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Casing 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Casing 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+        </ul>
+    </div>
+        `;
+    }
+    else if (index == 9){
+        document.getElementById("stages").innerHTML = 
+        `<div class="display" style="height: 700px;" id="stages">
+        <p class="title"> Choose Your PSU</p>
+        <ul>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>PSU 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>PSU 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>PSU 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>PSU 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+        </ul>
+    </div>
+        `;
+    }
+    else if (index == 10){
+        document.getElementById("stages").innerHTML = 
+        `<div class="display" style="height: 700px;" id="stages">
+        <p class="title"> Choose Your Cooler</p>
+        <ul>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Cooler 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Cooler 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Cooler 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+            <li >
+                <a href="" class="menu">
+                    <img src="./Images/dummy.png" alt="" style="width: 120px;">
+                    <div class="text">
+                        <p>Cooler 1</p>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nam odit mollitia doloribus repudiandae delectus aspernatur, asperiores officia tempore labore id vitae sint error nobis aliquam repellendus sunt quae dignissimos.</p>
+                        <p class="price">Rp. 5.000.000,-</p>
+                    </div>
+                </a>   
+            </li>
+        </ul>
+    </div>
+        `;
+    }
+    index++;
 }
